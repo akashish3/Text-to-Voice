306fe53f4abbaf0ecbea096ba5ea6de51be61722
Ashish kumar <ashiofficial629@gmail.com>
2025-02-02 14:19:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function convert(){
     let input=document.getElementById("inputtext").value;
     if(input.trim()===""){
         alert("Type something ");
+        return;
     }
     let voice = new SpeechSynthesisUtterance(input);
     window.speechSynthesis.speak(voice);
